0da52dd87bf4c6613311ca6ba9c3b22e556f137d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-02 12:36:22 +0300

    Import NativeWind in project

--- client/App.js
+++ client/App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex w-full h-full items-center justify-center">
+      <Text className="text-red-500 text-3xl">Test.</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
