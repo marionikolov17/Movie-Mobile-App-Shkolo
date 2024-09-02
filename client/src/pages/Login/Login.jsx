c2eede6b782ba7bd21c6756184b398dfea88fdc3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-03 01:07:36 +0300

    Implement loading on login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,14 +1,16 @@
-import { SafeAreaView, Text } from "react-native";
+import { ActivityIndicator, SafeAreaView, Text } from "react-native";
 import AppHeader from "../../features/layout/components/AppHeader/AppHeader";
 import { LoginButton, AccessToken } from "react-native-fbsdk-next";
 import { Settings } from "react-native-fbsdk-next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import auth from "@react-native-firebase/auth";
 import { useDispatch } from "react-redux";
 import { authenticate } from "../../entities/users/reducers/userSlice";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const dispatch = useDispatch();
   const navigation = useNavigation();
 
@@ -27,8 +29,9 @@ export default function Login() {
         >
           Log In
         </Text>
-        <LoginButton
+        {!isLoading && <LoginButton
           onLoginFinished={async (error, result) => {
+            setIsLoading(true);
             if (error) {
               console.log("err", error);
             } else if (result.isCancelled) {
@@ -45,15 +48,18 @@ export default function Login() {
                 const obj = {
                   profile: user.additionalUserInfo.profile
                 }
-                console.log(user);
+                //console.log(user);
                 dispatch(authenticate(obj));
+                setIsLoading(false);
                 return navigation.navigate("Home")
               } catch (error) {
                 console.log("login err", error);
+                setIsLoading(false);
               }
             }
           }}
-        />
+        />}
+        {isLoading && <ActivityIndicator size="small" style={{ marginTop: 10 }}/>}
       </SafeAreaView>
     </>
   );
