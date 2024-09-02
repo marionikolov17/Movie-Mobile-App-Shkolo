b844fd8350e39c1f56111ee257cad04a91f602de
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-02 17:34:15 +0300

    Create route for login page

--- client/App.js
+++ client/App.js
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Navigation from "./src/features/layout/components/Navigation/Navigation";
 import MovieDetails from "./src/pages/MovieDetails/MovieDetails";
+import Login from "./src/pages/Login/Login";
 
 const Stack = createNativeStackNavigator();
 
@@ -37,6 +38,7 @@ export default function App() {
       >
         <Stack.Screen name="Home" component={Navigation}/>
         <Stack.Screen name="MovieDetails" component={MovieDetails}/>
+        <Stack.Screen name="Login" component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
