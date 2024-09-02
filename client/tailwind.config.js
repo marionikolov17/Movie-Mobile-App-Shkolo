913f1043f1955a1f56713379c4ace598c03f1dbd
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-02 13:02:50 +0300

    Add custom color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBackground: "#E8E8E8",
+        primaryBlue: "#5D5FEF",
+        blueGray: "#737791",
+        primaryGray: "#F9FAFB",
+        supportingYellow: "#FFA412",
+        secondaryYellow: "#FFFAF1",
+        strongRed: "#EB5757"
+      },
+    },
   },
   plugins: [],
 }
