7aab768ce2a855f8af526b3a082c67800da4df6c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-03 14:37:03 +0300

    Set notifications

--- client/App.js
+++ client/App.js
@@ -1,9 +1,12 @@
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import {PermissionsAndroid, Linking} from 'react-native';
 import { Provider } from "react-redux";
 import { useFonts } from "expo-font";
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import messaging from "@react-native-firebase/messaging";
+
 import Navigation from "./src/features/layout/components/Navigation/Navigation";
 import MovieDetails from "./src/pages/MovieDetails/MovieDetails";
 import Login from "./src/pages/Login/Login";
@@ -13,11 +16,89 @@ const Stack = createNativeStackNavigator();
 
 SplashScreen.preventAutoHideAsync();
 
+const NAVIGATION_IDS = ['home', 'movieDetails'];
+
+function buildDeepLinkFromNotificationData(data) {
+  const navigationId = data?.navigationId;
+  if (!NAVIGATION_IDS.includes(navigationId)) {
+    console.warn('Unverified navigationId', navigationId)
+    return null;
+  }
+  if (navigationId === 'home') {
+    return 'myapp://home';
+  }
+  const movieId = data?.movieId;
+  if (typeof movieId === 'string') {
+    return `myapp://movieDetails/${movieId}`
+  }
+  console.warn('Missing movieId')
+  return null
+}
+
+const linking = {
+  prefixes: ['myapp://'],
+  config: {
+    initialRouteName: 'Home',
+    screens: {
+      Home: 'home',
+      MovieDetails: 'movieDetails/:id',
+    }
+  },
+  async getInitialURL() {
+    const url = await Linking.getInitialURL();
+    if (typeof url === 'string') {
+      return url;
+    }
+    //getInitialNotification: When the application is opened from a quit state.
+    const message = await messaging().getInitialNotification();
+    const deeplinkURL = buildDeepLinkFromNotificationData(message?.data);
+    if (typeof deeplinkURL === 'string') {
+      return deeplinkURL;
+    }
+  },
+  subscribe(listener) {
+    const onReceiveURL = ({url}) => listener(url);
+
+    // Listen to incoming links from deep linking
+    const linkingSubscription = Linking.addEventListener('url', onReceiveURL);
+
+    //onNotificationOpenedApp: When the application is running, but in the background.
+    const unsubscribe = messaging().onNotificationOpenedApp(remoteMessage => {
+      const url = buildDeepLinkFromNotificationData(remoteMessage.data)
+      if (typeof url === 'string') {
+        listener(url)
+      }
+    });
+
+    return () => {
+      linkingSubscription.remove();
+      unsubscribe();
+    };
+  },
+}
+
 export default function App() {
   const [loaded, error] = useFonts({
     Montserrat: require("./assets/fonts/Montserrat.ttf"),
     MontserratBold: require("./assets/fonts/Montserrat-Bold.ttf")
   });
+
+  /* Request notifications permissions */
+  useEffect(() => {
+    async function requestUserPermission() {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    }
+
+    requestUserPermission();
+  }, [])
 
   useEffect(() => {
     if (loaded || error) {
@@ -31,7 +112,7 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer linking={linking}>
         <Stack.Navigator
           screenOptions={
             {
