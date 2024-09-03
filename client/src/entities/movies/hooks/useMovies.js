97301831bfac06ad3ae3842f62f441c3987ea41e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-03 12:15:35 +0300

    Create useMovies.js

---
+++ client/src/entities/movies/hooks/useMovies.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import * as movieService from "./../services/movie.service";
+
+export const useGetAllMovies = () => {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        (async () => {
+            try {
+                const response = await movieService.getAllMovies();
+                const movies = await response.json();
+
+                setMovies(movies);
+            } catch (error) {
+                setError(error)
+            } finally {
+                setIsLoading(false);
+            }
+        })()
+    }, []);
+
+    return { movies, isLoading, error }
+}
+
+export const useGetMovie = (id) => {
+    const [movie, setMovie] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        (async () => {
+            try {
+                const response = await movieService.getMovie(id);
+                const movie = await response.json();
+
+                setMovie(movie);
+            } catch (error) {
+                setError(error)
+            } finally {
+                setIsLoading(false);
+            }
+        })()
+    }, []);
+
+    return { movie, isLoading, error }
+}
