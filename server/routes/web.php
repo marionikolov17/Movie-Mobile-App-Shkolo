a862e83f39596e2caa1b1de80a2929d3da8527a3
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-03 12:00:27 +0300

    Finish laravel back-end

--- server/routes/web.php
+++ server/routes/web.php
@@ -2,6 +2,64 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/movies', function () {
+    $movies = [
+        [
+            'id' => 1,
+            'name' => 'Deadpool 3',
+            'year' => 2024,
+            'rating' => 5,
+            'totalVotes' => 4769,
+            'resume' => 'test resume',
+            'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
+            'genre' => 'Action',
+            'hashtags' => ['Action', 'Marvel']
+        ],
+        [
+            'id' => 2,
+            'name' => 'Deadpool 2',
+            'year' => 2024,
+            'rating' => 5,
+            'totalVotes' => 4769,
+            'resume' => 'test',
+            'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
+            'genre' => 'Action',
+            'hashtags' => ['Action', 'Marvel']
+        ],
+    ];
+
+    return response()->json($movies);
+});
+
+Route::get('/movies/{id}', function ($id) {
+    $movies = [
+        1 => [
+            'id' => 1,
+            'name' => 'Deadpool 3',
+            'year' => 2024,
+            'rating' => 5,
+            'totalVotes' => 4769,
+            'resume' => 'test resume',
+            'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
+            'genre' => 'Action',
+            'hashtags' => ['Action', 'Marvel']
+        ],
+        2 => [
+            'id' => 2,
+            'name' => 'Deadpool 2',
+            'year' => 2024,
+            'rating' => 5,
+            'totalVotes' => 4769,
+            'resume' => 'test',
+            'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
+            'genre' => 'Action',
+            'hashtags' => ['Action', 'Marvel']
+        ],
+    ];
+
+    if (array_key_exists($id, $movies)) {
+        return response()->json($movies[$id]);
+    } else {
+        return response()->json(['message' => 'Movie not found'], 404);
+    }
 });
