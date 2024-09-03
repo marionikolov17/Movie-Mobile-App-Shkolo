95e514b94d4ff1d6b2334bd6a4a705e7f708eb8a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-09-03 13:05:34 +0300

    Create Movie details page dynamic

--- server/routes/web.php
+++ server/routes/web.php
@@ -10,21 +10,23 @@ Route::get('/movies', function () {
             'year' => 2024,
             'rating' => 5,
             'totalVotes' => 4769,
-            'resume' => 'test resume',
+            'resume' => 'Deadpool is offered a place in the Marvel Cinematic Universe by the Time Variance Authority, but instead recruits a variant of Wolverine to save his universe from extinction.',
             'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
             'genre' => 'Action',
-            'hashtags' => ['Action', 'Marvel']
+            'hashtags' => ['Action', 'Marvel'],
+            'createdAt' => '2024-08-22'
         ],
         [
             'id' => 2,
-            'name' => 'Deadpool 2',
-            'year' => 2024,
+            'name' => 'Spider-Man',
+            'year' => 2022,
             'rating' => 5,
-            'totalVotes' => 4769,
-            'resume' => 'test',
-            'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
+            'totalVotes' => 47696,
+            'resume' => "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear.",
+            'imageUrl' => 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/Cu9fyu6DM41JPekXLf1neF9r.png',
             'genre' => 'Action',
-            'hashtags' => ['Action', 'Marvel']
+            'hashtags' => ['Action', 'Spiderman'],
+            'createdAt' => '2024-06-20'
         ],
     ];
 
@@ -39,21 +41,23 @@ Route::get('/movies/{id}', function ($id) {
             'year' => 2024,
             'rating' => 5,
             'totalVotes' => 4769,
-            'resume' => 'test resume',
+            'resume' => 'Deadpool is offered a place in the Marvel Cinematic Universe by the Time Variance Authority, but instead recruits a variant of Wolverine to save his universe from extinction.',
             'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
             'genre' => 'Action',
-            'hashtags' => ['Action', 'Marvel']
+            'hashtags' => ['Action', 'Marvel'],
+            'createdAt' => '2024-08-22'
         ],
         2 => [
             'id' => 2,
-            'name' => 'Deadpool 2',
-            'year' => 2024,
+            'name' => 'Spider-Man',
+            'year' => 2022,
             'rating' => 5,
-            'totalVotes' => 4769,
-            'resume' => 'test',
-            'imageUrl' => 'https://deadline.com/wp-content/uploads/2024/01/Deadpool.jpg?w=681&h=383&crop=1',
+            'totalVotes' => 47696,
+            'resume' => "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear.",
+            'imageUrl' => 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/Cu9fyu6DM41JPekXLf1neF9r.png',
             'genre' => 'Action',
-            'hashtags' => ['Action', 'Marvel']
+            'hashtags' => ['Action', 'Spiderman'],
+            'createdAt' => '2024-06-20'
         ],
     ];
 
